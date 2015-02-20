62a0cb75ac7164cf4a2cf0a65419331f096f524e
oeuillot <olivier.oeuillot@gmail.com>
2015-02-20 15:06:44 +0100

    First tests

--- index.js
+++ index.js
@@ -19,10 +19,11 @@ omxplayer._sessionBus.listNames(function(error, list) {
 
 });
 
-omxPlayerService.getInterface('/org/mpris/MediaPlayer2', function(error, omxPlayerNotificationsInterface) {
-	if (error) {
-		console.error("Get interface error:", error);
-		return;
-	}
-	console.log("Omx player interface=", omxPlayerNotificationsInterface);
-});
+omxplayer._sessionBus.getInterface('org.mpris.MediaPlayer2.omxplayer', '/org/mpris/MediaPlayer2',
+		'org.freedesktop.DBus.Properties.Position', function(error, omxPlayerNotificationsInterface) {
+			if (error) {
+				console.error("Get interface error:", error);
+				return;
+			}
+			console.log("Omx player interface=", omxPlayerNotificationsInterface);
+		});
