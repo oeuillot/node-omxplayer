db8fa4c2dea18992e358dd1a7fe7ad1fd90e114e
oeuillot <olivier.oeuillot@gmail.com>
2015-02-20 14:43:31 +0100

    First tests

--- index.js
+++ index.js
@@ -8,8 +8,8 @@ var omxplayer = new OMXPlayer(configuration);
 omxplayer.start("movie.mkv");
 
 var omxPlayerService = omxplayer._sessionBus.getService('org.mpris.MediaPlayer2.omxplayer');
-console.debug("Omx player service=", omxPlayerService);
+console.log("Omx player service=", omxPlayerService);
 
 var omxPlayerNotificationsInterface = omxPlayerService.getInterface('/org/mpris/MediaPlayer2',
 		'org.freedesktop.Notifications');
-console.debug("Omx player interface=", omxPlayerNotificationsInterface);
+console.log("Omx player interface=", omxPlayerNotificationsInterface);
