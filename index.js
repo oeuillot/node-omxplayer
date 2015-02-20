c8995db29bd6ac954f3fca7c2693db246b653733
oeuillot <olivier.oeuillot@gmail.com>
2015-02-20 17:50:38 +0100

    First tests

--- index.js
+++ index.js
@@ -7,22 +7,14 @@ var configuration = {
 var omxplayer = new OMXPlayer(configuration);
 omxplayer.start("movie.mkv");
 
-var omxPlayerService = omxplayer._sessionBus.getService('org.mpris.MediaPlayer2.omxplayer');
-console.log("Omx player service=", omxPlayerService);
-
-omxplayer._sessionBus.listNames(function(error, list) {
-	if (error) {
-		console.error("Get interface error:", error);
-		return;
-	}
-	console.log("Omx player listNames=", list);
-
-});
-
-omxPlayerService.getObject('org.mpris.MediaPlayer2.omxplayer', function(error, omxPlayerNotificationsInterface) {
-	if (error) {
-		console.error("Get interface error:", error);
-		return;
-	}
-	console.log("Omx player interface=", omxPlayerNotificationsInterface);
-});
+omxplayer._sessionBus.getInterface('org.mpris.MediaPlayer2.omxplayer', '/org/mpris/MediaPlayer2', 'interface',
+		function(error, iface) {
+			if (error) {
+				console.error("Get interface error:", error);
+				return;
+			}
+			iface.getProperties(function(error, props) {
+				console.log('Properties:');
+				console.log(props);
+			});
+		});
