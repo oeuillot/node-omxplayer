46e1f643d6ba05e7a4e38702411f4b8f233c6663
oeuillot <olivier.oeuillot@gmail.com>
2015-02-20 13:52:37 +0100

    First tests

---
+++ index.js
@@ -0,0 +1,15 @@
+var OMXPlayer = require('./lib/omxplayer');
+
+var configuration = {
+
+};
+
+var omxplayer = new OMXPlayer(configuration);
+omxplayer.start(path);
+
+var omxPlayerService = sessionBus.getService('org.mpris.MediaPlayer2.omxplayer');
+console.debug("Omx player service=", omxPlayerService);
+
+var omxPlayerNotificationsInterface = omxPlayerService.getInterface('/org/mpris/MediaPlayer2',
+		'org.freedesktop.Notifications');
+console.debug("Omx player interface=", omxPlayerNotificationsInterface);
