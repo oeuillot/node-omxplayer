bd6fb4b42610148b8515a4a92244dc00954b69fc
oeuillot <olivier.oeuillot@gmail.com>
2015-02-25 12:53:12 +0100

    First tests

--- index.js
+++ index.js
@@ -1,41 +1,44 @@
 var OMXPlayer = require('./lib/omxplayer');
+var commander = require('commander');
 
-var configuration = {};
+command.command('node-omxplayer <filename> [filenames ...]');
 
-var omxplayer = new OMXPlayer(configuration);
-console.log(omxplayer);
-omxplayer.start("movie.mkv", function(error) {
-	if (error) {
-		console.error("Start: ", error);
-		return;
+commander.version(require("./package.json").version);
+
+commander.option("-b, --blank", "Set background to black");
+commander.option("-o, --adev <device>", "Audio out device");
+commander.option("-p, --passthrough", "Audio passthrough");
+commander.option("-d, --deinterlace", "Deinterlacing");
+commander.option("-y, --hdmiclocksync", "Display refresh rate to match video");
+commander.option("-z, --nohdmiclocksync", "Do not adjust display refresh rate to match video");
+commander.option("--timeout <float>", "Timeout for stalled file/network operations (seconds)", parseFloat);
+commander.option("--orientation <int>", "Set orientation of video (0, 90, 180 or 270)", parseInt);
+// commander.option(" --loop", "Loop file. Ignored if file not seekable");
+commander.option("--vol <volume>", "Set initial volume in millibels (default 0)", parseFloat);
+commander.option("--path <omxPlayerPath>", "Path of omxplayer");
+
+commander.action(function(fileName, filenames) {
+
+	var omxplayer = new OMXPlayer(commander);
+
+	var list = arguments.splice(0);
+
+	function start() {
+		var next = list.shift();
+
+		console.log("Start movie", next);
+
+		omxplayer.start(next, function(error) {
+			if (error) {
+				console.error("Start: ", error);
+				return;
+			}
+		});
 	}
 
-	/*
-	setInterval(function() {
+	omxplayer.on("stopped", start);
 
-		omxplayer.getPosition(function(error, position) {
-			if (error) {
-				console.error("GetPosition returns error", error);
-				return;
-			}
-			console.log("Position=", position);
-		});
-
-	}, 1000);
-
-	setInterval(function() {
-
-		omxplayer.getVolume(function(error, volume) {
-			if (error) {
-				console.error("GetPosition returns error", error);
-				return;
-			}
-			console.log("Volume=", volume);
-
-			omxplayer.setVolume(volume + 1, function(error, v) {
-				console.error("SetVolume returns ", v);
-			});
-		});
-
-	}, 10000);*/
+	start();
 });
+
+commander.parse(process.argv);
